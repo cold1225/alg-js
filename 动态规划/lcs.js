d1aa2a30fdf9889cdc204b4c1c4eea4aa1257065
王寒 <wanghan@sina.com>
2021-09-09 18:26:32 +0800

    lcs

--- 动态规划/lcs.js
+++ 动态规划/lcs.js
@@ -1,3 +1,4 @@
+// 递归解法
 function lcs(s, t) {
     if(s === '' || t === '') {
         return 0
@@ -14,4 +15,26 @@ function lcs(s, t) {
         )
     }
 }
-console.log(lcs('BANANA', 'ATANA'))
+// 动态规划 填表法
+
+function dp_lcs(s, t) {
+    const dp = Array.from({length: s.length + 1}, () => Array(t.length + 1))
+    for(let i = 0; i < dp.length; i++) {
+        dp[i][0] = 0
+    }
+    for(let j = 0; j < dp[0].length; j++) {
+        dp[0][j] = 0
+    }
+    for(let k = 1; k < dp.length; k++) {
+        for(let m = 1; m < dp[0].length; m++) {
+            if (s[k - 1] === t[m - 1]) {
+                dp[k][m] = dp[k - 1][m - 1] + 1
+            } else {
+                dp[k][m] = Math.max(dp[k - 1][m], dp[k][m - 1])
+            }
+        }
+    }
+    return dp[dp.length - 1][dp[0].length - 1]
+}
+
+// console.log(dp_lcs('BANANA', 'ATANA'))
